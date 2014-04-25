37858497862895446fb3a0b808ebf94375a42420
Shawna Mattison <shawnamatt@gmail.com>
2014-04-24 22:00:24 -0700

    get plain text article from wikipedia from search string

---
+++ bin/sparki_junior.rb
@@ -0,0 +1,48 @@
+require 'rest-client'
+require 'addressable/uri'
+require 'launchy'
+
+# https://en.wikipedia.org/w/api.php?action=opensearch&search=api&limit=10&namespace=0&format=jsonfm
+
+def build_search_uri(search_string)
+  search_uri = Addressable::URI.new(
+  scheme: "http",
+  host: "en.wikipedia.org",
+  path: "w/api.php",
+  query_values: {
+    action: "opensearch",
+    search: search_string,
+    limit: "10",
+    format: "json"
+    }).to_s
+end
+
+def get_article_title_from_search(search_string)
+  search_response = RestClient.get(build_search_uri(search_string))
+  search_response = JSON.parse(search_response)
+  
+  unless search_response[1].empty?
+    search_response[1][0]
+  end
+  
+  nil
+end
+
+def build_article_uri(title)
+  Addressable::URI.new(
+    scheme: "http",
+    host: "en.wikipedia.org",
+    path: "w/api.php",
+    query_values: {
+      action: "query",
+      titles: title,
+      format: "json",
+      prop: "extracts",
+      explaintext: "true"
+      }).to_s
+end
+
+  
+if __FILE__ == $PROGRAM_NAME
+  Launchy.open(build_article_uri("Abraham Lincoln"))
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sparki::Application.config.secret_key_base = 'be62eb350d7880803e878c006f0ec8a7f228a2b5a4f6a5fef1b08c0af4fb1aa171ad2e06be241743ede6e0ba4e91e30002d424817fdb46ca71fb4fb22a57128f'
