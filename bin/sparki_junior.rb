42d694d51aa41a61a2bf6a02dc14ff40d7ffb627
Shawna <shawna@appacademy.io>
2014-09-30 14:47:51 -0700

    put search and article queries together

--- bin/sparki_junior.rb
+++ bin/sparki_junior.rb
@@ -1,6 +1,7 @@
 require 'rest-client'
 require 'addressable/uri'
 require 'launchy'
+require 'json'
 
 # https://en.wikipedia.org/w/api.php?action=opensearch&search=api&limit=10&namespace=0&format=jsonfm
 
@@ -20,11 +21,11 @@ end
 def get_article_title_from_search(search_string)
   search_response = RestClient.get(build_search_uri(search_string))
   search_response = JSON.parse(search_response)
-  
+
   unless search_response[1].empty?
-    search_response[1][0]
+    return search_response[1][0]
   end
-  
+
   nil
 end
 
@@ -42,7 +43,8 @@ def build_article_uri(title)
       }).to_s
 end
 
-  
+
 if __FILE__ == $PROGRAM_NAME
-  Launchy.open(build_article_uri("Abraham Lincoln"))
+  search_string = get_article_title_from_search("Abraham Lincoln")
+  Launchy.open(build_article_uri(search_string))
 end
